b85d48428d868c58a936cb3d2f941de797726011
kartud <1075238+kartud@users.noreply.github.com>
2022-10-11 17:41:34 -0700

    Some db logging info

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,11 +1,9 @@
 const mongoose = require('mongoose');
 
-
-
 const tryConnection = () => {
     mongoose.connect(process.env.MONGODB_URL)
         .catch(error => {
-            console.error('Error connecting to database: ', error);
+            console.error(`Error connecting to database at ${process.env.MONGODB_URL}: `, error);
             setTimeout(tryConnection, 60 * 1000);
         })
 }
