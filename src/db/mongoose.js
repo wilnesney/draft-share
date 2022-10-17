70155ca81e76bdaa126e3e9cfced05bc4ab155b3
kartud <1075238+kartud@users.noreply.github.com>
2022-10-17 14:32:31 -0700

    Exit application if can't connect to db at startup. Also removed an obsolete comment about icons.

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,11 +1,9 @@
 const mongoose = require('mongoose');
 
-const tryConnection = () => {
-    mongoose.connect(process.env.MONGODB_URL)
-        .catch(error => {
-            console.error(`Error connecting to database at ${process.env.MONGODB_URL}: `, error);
-            setTimeout(tryConnection, 60 * 1000);
-        })
-}
-
-tryConnection();
+mongoose.connect(process.env.MONGODB_URL)
+    .catch(error => {
+        // If we can't connect to the database at startup, exit the application. 
+        // Process management should restart (and thus retry) in production.
+        console.error(`Exiting. Error connecting to database at ${process.env.MONGODB_URL}: `, error);
+        process.exit(1);
+    });
